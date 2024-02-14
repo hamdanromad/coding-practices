a2360c599a03577709f0cb96f4e82ae9eb555ef2
hamdanromad <hamdanromadhona@gmail.com>
2024-02-14 22:22:19 +0700

    add new practice

---
+++ diamond-pattern.py
@@ -0,0 +1,10 @@
+"""
+Create a diamond pattern
+"""
+
+h = int(input("Please enter diamond's height:"))
+
+for i in range(1, h, 2):
+    print(" "*(h//2-i//2), "*"*i)
+for i in range(h, 0, -2):
+    print(" "*(h//2-i//2), "*"*i)
