6b92e155249d7f2e6880a4d1d9e4ac282d5ea73f
hamdanromad <hamdanromadhona@gmail.com>
2024-02-25 21:33:47 +0700

    make into function

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,17 @@
-# Fibonacci
-input = 10
-a = 1
-b = 1
+'''
+Buat program untuk generate deret fibbonaci, 
+misal input 5 output  0, 1, 1, 2, 3, 5
+'''
+def fibbonaci(input):
+    a = 0
+    b = 1
+    result = ''
+    for i in range(input+1):
+        result += str(a) + ' '
+        temp = a
+        a = b
+        b = temp + b
+    return result
 
-result = ''
-for i in range(input+1):
-    result += str(a) + ' '
-    temp = a
-    a = b
-    b = temp + b
-print(result)
+input = 10
+print(fibbonaci(input))
